9b1dad6cc93bc7be00173cd559398f0d5bf11fa0
Jasmine Flores <jflores208@cnm.edu>
2021-02-23 17:10:41 -0700

    added logical statements

---
+++ index.js
@@ -0,0 +1,32 @@
+let hour = 19;
+let isWeekend = true;
+if  (hour < 7 || hour > 23 || isWeekend) {
+	document.write("I’m sleep");
+}
+
+
+
+let rain= true;
+let snow= false;
+let sun= true;
+if (rain===true || snow === false && sunny === true) {
+    document.write("Go Outside!")
+}
+
+
+let money = false;
+let isBroke = "Yes";
+let nameSpace = "test";
+document.write( money || isBroke || nameSpace || "What??" );
+Output = "What??";
+
+let x =0;
+
+x===0; true || document.write("is true");
+false || document.write("not true");
+
+
+
+
+
+
